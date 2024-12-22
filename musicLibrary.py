bb9d68ac9dc89bb9d1439409d304069544876e47
Krish8290 <krish722007@gmail.com>
2024-12-22 11:11:19 +0530

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "naina" : "https://www.youtube.com/watch?v=3u6lLWGjFLY&list=RDMM3u6lLWGjFLY&start_radio=1",
+    "shape" : "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "tenu" : "https://www.youtube.com/watch?v=6d5SS0gS5bU",
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI",
+}
